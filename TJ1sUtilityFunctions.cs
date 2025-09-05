24bdb8facb9d24bb29880935daea00ff61bfc453
Ubico <138281135+TJ1the0fficial@users.noreply.github.com>
2025-09-05 15:24:20 +0200

    First Commit
    Has print and input functions, for fun.

---
+++ TJ1sUtilityFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Learning_II
+{
+    public class AbdisUtilityFunctions
+    {
+        public static string input(params object[] text) {
+            Console.Write(string.Join(" ",text));
+            return Console.ReadLine();
+        }
+
+        public static void print(params object[] text) {
+            Console.WriteLine(string.Join(" ",text));
+        }
+
+    }
+    
+}
